13368b4534e964c08c0077b937980244e790b404
kryakobess <martyz89@yandex.ru>
2024-04-26 23:22:20 +0300

    Model prediction output

--- CupidService/src/ui/BeginnerForm/app.R
+++ CupidService/src/ui/BeginnerForm/app.R
@@ -67,6 +67,18 @@ ui <- navbarPage(
             radioButtons("gender", label = "Пол",
                          choices = list("М" = 1, "Ж" = 2), 
                          selected = 1),
+          
+          sliderInput("imprace",
+                      "Насколько для вас важно (по шкале от 1 до 10), чтобы человек, с которым вы встречаетесь, был того же расового/этнического происхождения?",
+                      min = 1,
+                      max = 10,
+                      value = 5),
+          
+          sliderInput("imprelig",
+                      "НасколькоНасколько для вас важно (по шкале от 1 до 10), чтобы человек, с которым вы встречаетесь, был того же религиозного происхождения?",
+                      min = 1,
+                      max = 10,
+                      value = 5),
             
           helpText("Оцените насколько ВАМ интересны следующие активности по шкале от 1 до 10"),
           
@@ -279,18 +291,29 @@ df = data.frame(
   intel_a = integer(),
   fun_a = integer(),
   amb_a = integer(),
-  shar_a = integer()
+  shar_a = integer(),
+  imprace = integer(),
+  imprelig = integer()
 )
 
-#wf_rf4 = fitModel()
+wf_rf4 = fitModel()
 
-calcConnectivity <- function(input_test) {
-  predtest.rf4_prob = predict(wf_rf4, input_test, type = "prob")
-  return(predtest.rf4_prob$.pred_1)
+getPartnerInterestsArray <- function(input_df, i) {
+  return(
+    c(as.numeric(input_df[i,]$sports), as.numeric(input_df[i,]$tv_sports), as.numeric(input_df[i,]$exercise),
+      as.numeric(input_df[i,]$gaming), as.numeric(input_df[i,]$clubbing), as.numeric(input_df[i,]$reading), as.numeric(input_df[i,]$shopping),
+      as.numeric(input_df[i,]$yoga))
+  )
 }
 
-parseInput <- function(input_df) {
-  print(as.numeric(input_df[1,]$age))
+calcIntCor <- function(input_df) {
+  first_int = getPartnerInterestsArray(input_df, 1)
+  second_int = getPartnerInterestsArray(input_df, 2)
+  cor_res = cor(first_int, second_int)
+  return(ifelse(is.na(cor_res), 0, cor_res))
+}
+
+mapInputDfToDiff <- function(input_df) {
   diff_df = data.frame(
     int_corr = integer(),
     age_dif = integer(),
@@ -317,29 +340,38 @@ parseInput <- function(input_df) {
     amb2_1_dif = integer(),
     shar2_1_dif = integer()
   )
-  diff_df[1,]$age_dif = abs(as.numeric(input_df[1,]$age) - as.numeric(input_df[1,]$age))
-  diff_df[1,]$sports_dif = abs(as.numeric(input_df[1,]$sports) - as.numeric(input_df[1,]$sports))
-  diff_df[1,]$tvsports_dif = abs(as.numeric(input_df[1,]$tv_sports) - as.numeric(input_df[1,]$tv_sports))
-  diff_df[1,]$exercise_dif = abs(as.numeric(input_df[1,]$exercise) - as.numeric(input_df[1,]$exercise))
-  diff_df[1,]$gaming_dif = abs(as.numeric(input_df[1,]$gaming) - as.numeric(input_df[1,]$gaming))
-  diff_df[1,]$clubbing_dif = abs(as.numeric(input_df[1,]$clubbing) - as.numeric(input_df[1,]$clubbing))
-  diff_df[1,]$reading_dif = abs(as.numeric(input_df[1,]$reading) - as.numeric(input_df[1,]$reading))
-  diff_df[1,]$shopping_dif = abs(as.numeric(input_df[1,]$shopping) - as.numeric(input_df[1,]$shopping))
-  diff_df[1,]$yoga_dif = abs(as.numeric(input_df[1,]$yoga) - as.numeric(input_df[1,]$yoga))
-  diff_df[1,]$attr1_1_dif = abs(as.numeric(input_df[1,]$attr) - as.numeric(input_df[1,]$attr))
-  diff_df[1,]$sinc1_1_dif = abs(as.numeric(input_df[1,]$sinc) - as.numeric(input_df[1,]$sinc))
-  diff_df[1,]$intel1_1_dif = abs(as.numeric(input_df[1,]$intel) - as.numeric(input_df[1,]$intel))
-  diff_df[1,]$fun1_1_dif = abs(as.numeric(input_df[1,]$fun) - as.numeric(input_df[1,]$fun))
-  diff_df[1,]$amb1_1_dif = abs(as.numeric(input_df[1,]$amb) - as.numeric(input_df[1,]$amb))
-  diff_df[1,]$shar1_1_dif = abs(as.numeric(input_df[1,]$shar) - as.numeric(input_df[1,]$shar))
-  diff_df[1,]$attr2_1_dif = abs(as.numeric(input_df[1,]$attr_a) - as.numeric(input_df[1,]$attr_a))
-  diff_df[1,]$sinc2_1_dif = abs(as.numeric(input_df[1,]$sinc_a) - as.numeric(input_df[1,]$sinc_a))
-  diff_df[1,]$intel2_1_dif = abs(as.numeric(input_df[1,]$intel_a) - as.numeric(input_df[1,]$intel_a))
-  diff_df[1,]$fun2_1_dif = abs(as.numeric(input_df[1,]$fun_a) - as.numeric(input_df[1,]$fun_a))
-  diff_df[1,]$amb2_1_dif = abs(as.numeric(input_df[1,]$amb_a) - as.numeric(input_df[1,]$amb_a))
-  diff_df[1,]$shar2_1_dif = abs(as.numeric(input_df[1,]$shar_a) - as.numeric(input_df[1,]$shar_a))
   
-  print(diff_df)
+  diff_df[1,]$int_corr = calcIntCor(input_df)
+  diff_df[1,]$age_dif = abs(as.numeric(input_df[1,]$age) - as.numeric(input_df[2,]$age))
+  diff_df[1,]$imprace_dif = abs(as.numeric(input_df[1,]$imprace) - as.numeric(input_df[2,]$imprace))
+  diff_df[1,]$imprelig_dif = abs(as.numeric(input_df[1,]$imprelig) - as.numeric(input_df[2,]$imprelig))
+  diff_df[1,]$sports_dif = abs(as.numeric(input_df[1,]$sports) - as.numeric(input_df[2,]$sports))
+  diff_df[1,]$tvsports_dif = abs(as.numeric(input_df[1,]$tv_sports) - as.numeric(input_df[2,]$tv_sports))
+  diff_df[1,]$exercise_dif = abs(as.numeric(input_df[1,]$exercise) - as.numeric(input_df[2,]$exercise))
+  diff_df[1,]$gaming_dif = abs(as.numeric(input_df[1,]$gaming) - as.numeric(input_df[2,]$gaming))
+  diff_df[1,]$clubbing_dif = abs(as.numeric(input_df[1,]$clubbing) - as.numeric(input_df[2,]$clubbing))
+  diff_df[1,]$reading_dif = abs(as.numeric(input_df[1,]$reading) - as.numeric(input_df[2,]$reading))
+  diff_df[1,]$shopping_dif = abs(as.numeric(input_df[1,]$shopping) - as.numeric(input_df[2,]$shopping))
+  diff_df[1,]$yoga_dif = abs(as.numeric(input_df[1,]$yoga) - as.numeric(input_df[2,]$yoga))
+  diff_df[1,]$attr1_1_dif = abs(as.numeric(input_df[1,]$attr) - as.numeric(input_df[2,]$attr))
+  diff_df[1,]$sinc1_1_dif = abs(as.numeric(input_df[1,]$sinc) - as.numeric(input_df[2,]$sinc))
+  diff_df[1,]$intel1_1_dif = abs(as.numeric(input_df[1,]$intel) - as.numeric(input_df[2,]$intel))
+  diff_df[1,]$fun1_1_dif = abs(as.numeric(input_df[1,]$fun) - as.numeric(input_df[2,]$fun))
+  diff_df[1,]$amb1_1_dif = abs(as.numeric(input_df[1,]$amb) - as.numeric(input_df[2,]$amb))
+  diff_df[1,]$shar1_1_dif = abs(as.numeric(input_df[1,]$shar) - as.numeric(input_df[2,]$shar))
+  diff_df[1,]$attr2_1_dif = abs(as.numeric(input_df[1,]$attr_a) - as.numeric(input_df[2,]$attr_a))
+  diff_df[1,]$sinc2_1_dif = abs(as.numeric(input_df[1,]$sinc_a) - as.numeric(input_df[2,]$sinc_a))
+  diff_df[1,]$intel2_1_dif = abs(as.numeric(input_df[1,]$intel_a) - as.numeric(input_df[2,]$intel_a))
+  diff_df[1,]$fun2_1_dif = abs(as.numeric(input_df[1,]$fun_a) - as.numeric(input_df[2,]$fun_a))
+  diff_df[1,]$amb2_1_dif = abs(as.numeric(input_df[1,]$amb_a) - as.numeric(input_df[2,]$amb_a))
+  diff_df[1,]$shar2_1_dif = abs(as.numeric(input_df[1,]$shar_a) - as.numeric(input_df[2,]$shar_a))
+  
+  return(diff_df)
+}
+
+calcConnectivity <- function(input_test) {
+  predtest.rf4_prob = predict(wf_rf4, input_test, type = "prob")
+  return(predtest.rf4_prob$.pred_1)
 }
 
 server <- function(input, output) {
@@ -361,16 +393,21 @@ server <- function(input, output) {
       input$age, input$gender, input$sports, input$tv_sports, input$exercise, input$gaming, input$clubbing, input$reading, 
       input$shopping, input$yoga, input$museums, input$art, input$hiking, input$tv, input$theater, input$movies, input$concerts, input$music,
       input$attr, input$sinc, input$intel, input$fun, input$amb, input$shar, 
-      input$attr_a, input$sinc_a, input$intel_a, input$fun_a, input$amb_a, input$shar_a   
+      input$attr_a, input$sinc_a, input$intel_a, input$fun_a, input$amb_a, input$shar_a, input$imprace, input$imprelig
     )
     
     rv(newdf)
     df <<- rv()
     if (nrow(newdf) == 2) {
       shinyjs::hide(id = "conForm")
-      parseInput(newdf)
+      diff_df = mapInputDfToDiff(newdf)
+      pred_prob = calcConnectivity(diff_df) * 100
       output$conResult = renderText(
-        "ЗДЕСЬ БУДЕТ ВЫВОДИТЬСЯ РЕЗУЛЬТАТ"
+        paste(
+          "Ваш процент совместимости:\n",
+          pred_prob, "%",
+          sep=""
+          )
       )
     }
   })
